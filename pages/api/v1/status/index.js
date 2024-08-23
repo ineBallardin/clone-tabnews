cf39e432d70c87573372c67a9873e560469b8c8d
Ine <ineballardin@gmail.com>
2024-08-23 13:57:59 -0300

    chore(config): add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(req, res) {
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
   res.status(200).json({
     Status_da_API: "200",
   });
